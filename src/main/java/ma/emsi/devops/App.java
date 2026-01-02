01b161be7751ab5ce106fb22b29ba1d26cad638a
oussamatalibi <iks.coding10@gmail.com>
2026-01-02 15:35:41 +0100

    Add simple Java Maven app

---
+++ src/main/java/ma/emsi/devops/App.java
@@ -0,0 +1,15 @@
+package ma.emsi.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+
+
+    {
+        System.out.println("Bonjour et bon courage dans votre projet en DevOps");;
+    }
+}
